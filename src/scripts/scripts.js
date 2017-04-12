ebb9248bac61923262f7c733fa4f3cb2db3b27f5
André Lagacione <lagacioneandre@gmail.com>
2017-04-11 22:24:14 -0300

    function created for navigation in categories and subcategories and styles initialized for top application

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,13 +1,28 @@
-window.onload = function() {
-	actionsSidebarMenu();
-};
+function actionsSidebarMenu(item, e) {
+	var categories = document.querySelectorAll('#main-menu > ul > li'),
+		hasClass = item.className.split(' '),
+		allIconIndicator = document.querySelectorAll('.icon-sub-category i'),
+		iconIndicatorItem = item.querySelectorAll('.icon-sub-category i');
 
-var actionsSidebarMenu = function() {
-	var categoryItem = document.querySelectorAll('#main-menu > ul > li');
+	for (var i = 0; i < categories.length; i++) {
+		categories[i].classList.remove('active');
+	}
 
-	for (var i = 0; i < categoryItem.length; i++) {
-		categoryItem[i].addEventListener('click', function(e) {
-			categoryItem[i].classList.remove('active');
-		});
+	for (var i = 0; i < allIconIndicator.length; i++) {
+		allIconIndicator[i].classList.remove('fa-chevron-down');
+		allIconIndicator[i].classList.add('fa-chevron-right');
+	}
+
+	for (var i = 0; i < hasClass.length; i++) {
+		if (hasClass[i] == 'active') {
+			item.classList.remove('active');
+			iconIndicatorItem[0].classList.remove('fa-chevron-down');
+			iconIndicatorItem[0].classList.add('fa-chevron-right');
+			break;
+		} else {
+			item.classList.add('active');
+			iconIndicatorItem[0].classList.remove('fa-chevron-right');
+			iconIndicatorItem[0].classList.add('fa-chevron-down');
+		}
 	}
 }
