e6ac02effcb40492e15e124b413009af8f693229
André Lagacione <lagacioneandre@gmail.com>
2017-04-12 22:27:54 -0300

    get date and hour sript

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,4 +1,10 @@
-function actionsSidebarMenu(item, e) {
+window.onload = function loadWindow() {
+	var now = new Date();
+	getDateDate(now);
+	getHour(now);
+};
+
+function actionsSidebarMenu(item) {
 	var categories = document.querySelectorAll('#main-menu > ul > li'),
 		hasClass = item.className.split(' '),
 		allIconIndicator = document.querySelectorAll('.icon-sub-category i'),
@@ -25,4 +31,29 @@ function actionsSidebarMenu(item, e) {
 			iconIndicatorItem[0].classList.add('fa-chevron-down');
 		}
 	}
-}
+};
+
+function getDateDate(now) {
+	var day = now.getDate(),
+		month = now.getMonth() + 1,
+		year = now.getFullYear();
+
+	if (day < 10) day = '0' + day;
+	if (month < 10) month = '0' + month;
+
+	document.getElementById('day').innerHTML = day + '/' + month + '/' + year;
+};
+
+function getHour(now) {
+	setInterval(function() {
+		var hour = now.getHours(),
+		minute = now.getMinutes(),
+		seconds = now.getSeconds();
+
+	if (hour < 10) hour = '0' + hour;
+	if (minute < 10) minute = '0' + minute;
+	if (seconds < 10) seconds = '0' + seconds;
+
+	document.getElementById('hour').innerHTML = hour + ':' + minute + ':' + seconds;
+	}, 1000);
+};
