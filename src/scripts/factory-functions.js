97593f8a1297db88c0ca7a3cddfae859d05b0550
DESKTOP-IELO3K6\andre <lagacioneandre@gmail.com>
2017-05-29 22:36:45 -0300

    create factory-functions

---
+++ src/scripts/factory-functions.js
@@ -0,0 +1,56 @@
+// GLOBAL VARIABLES
+var intervals = {
+			category: null,
+			task: null
+		},
+		controlShowCategories = 0;
+
+window.onload = function loadWindow() {
+	var now = new Date();
+	getDateDate(now);
+	getHour();
+
+	setInterval(function() {
+		getHour();
+	}, 1000);
+
+	var contentApplication = document.getElementById('glass'),
+	    bodyTag = document.getElementsByTagName('body');
+
+	contentApplication.addEventListener('click', function() {
+		bodyTag[0].classList.remove('show-menu');
+		bodyTag[0].classList.add('hide-menu');
+		contentApplication.style.display = 'none';
+	});
+};
+
+function checkHasClass(element) {
+	return element.className.split(' ');
+}
+
+function removeActiveClass(element, classForRemove, classForAdd, classControl) {
+	for (var i = 0; i < element.length; i++) {
+		if (classForRemove && classForAdd) {
+			element[i].classList.remove(classForRemove);
+			element[i].classList.add(classForAdd);
+		} else {
+			element[i].classList.remove(classControl);
+		}
+	}
+}
+
+function checkRemoveAlertText(baseNode) {
+	var parentNodeChildrens = baseNode.parentNode.childNodes,
+			hasAlert = parentNodeChildrens[parentNodeChildrens.length - 1];
+
+	if (hasAlert && hasAlert.nodeName !== '#text') {
+		var hasClass = checkHasClass(hasAlert);
+
+		for (var i = 0; i < hasClass.length; i++) {
+			if (hasClass[i] == 'alert-text') {
+				hasAlert.remove();
+				break;
+			}
+		}
+	}
+}
